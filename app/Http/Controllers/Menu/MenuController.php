7470c6afb683b32491da4e1c71d23e310832823c
LilRookie69 <ridhoiyoy962013@gmail.com>
2021-03-02 01:59:55 +0700

    delete comment

--- app/Http/Controllers/Menu/MenuController.php
+++ app/Http/Controllers/Menu/MenuController.php
@@ -10,21 +10,6 @@ use Yajra\DataTables\Facades\DataTables;
 
 class MenuController extends Controller
 {
-    // public function getdatamenu()
-    // {
-    //     $menus = Menu::get();
-    //     return DataTables::of($menus)
-    //         ->addIndexColumn()wwww
-    //         ->addColumn('created_at', function ($menu) {
-    //             return $menu->created_at->diffForHumans();
-    //         })
-    //         ->addColumn('action', function ($menu) {
-    //             return '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $menu->slug . '" data-original-title="Edit" class="btn btn-sm btn-success btnEditMenu">Edit</a>
-    //             <a href="#" class="btn btn-sm btn-danger ml-1">Delete</a>';
-    //         })
-    //         ->rawColumns(['action', 'action'])
-    //         ->toJson();
-    // }
 
     public function index()
     {
@@ -44,23 +29,6 @@ class MenuController extends Controller
         }
         return view('backend.menus.menu');
     }
-
-
-    // public function store()
-    // {
-    //     request()->validate([
-    //         'nama' => 'required|unique:menus,nama',
-    //         'harga' => 'required',
-    //     ]);
-
-    //     Menu::create([
-    //         'nama' => request('nama'),
-    //         'slug' => Str::slug(request('nama')),
-    //         'harga' => request('harga')
-    //     ]);
-
-    //     return back()->with('success', 'Menu was Created');
-    // }
 
     public function store(Request $request, Menu $menu)
     {
@@ -85,26 +53,7 @@ class MenuController extends Controller
 
     public function edit($id)
     {
-        // $here = array('slug' => $slug);
-        // $data = Menu::where($here)->first();
         $data = Menu::findOrFail($id);
         return response()->json($data);
     }
-
-    // public function update(Menu $menu)
-    // {
-    //     request()->validate([
-    //         'nama' => 'required|unique:menus,nama' . $menu->slug,
-    //         'harga' => 'required',
-    //     ]);
-
-    //     $menu->update([
-    //         'nama' => request('nama'),
-    //         'slug' => Str::slug(request('nama')),
-    //         'harga' => request('harga')
-    //     ]);
-
-    //     // dd($menu);
-    //     return back()->with('success', 'Menu was Updated');
-    // }
 }
