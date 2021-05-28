f987ff736a3a8e71d2cf00556b02329fedceb944
LilRookie69 <ridhoiyoy962013@gmail.com>
2021-05-28 15:16:07 +0700

    reconfig

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,3 +10,10 @@ const mix = require("laravel-mix");
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.js("resources/js/app.js", "public/js").postCss(
+    "resources/css/app.css",
+    "public/css",
+    [
+        //
+    ]
+);
