296b1e1d88dcdebe5f27d4a28b87063c8f0a53be
LilRookie69 <ridhoiyoy962013@gmail.com>
2021-06-24 06:34:14 +0700

    [UPDATE] edit user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
 
@@ -23,6 +24,7 @@ class UserResource extends JsonResource
             'image_url' => $this->image ? Storage::disk('s3')->url($this->image) : 'https://via.placeholder.com/600',
             'roles' => $this->roles[0]->name,
             'role_id' => $this->roles[0]->id,
+            'created_at' => Carbon::parse($this->created_at)->format('j F, Y'),
         ];
     }
 }
