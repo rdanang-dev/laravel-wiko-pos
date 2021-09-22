50c94ea207a3c096098c656f8c4264c520559035
RDanang <rdanang.dev@gmail.com>
2021-09-22 14:18:45 +0700

    add name for some routes for better api documentation

--- routes/api.php
+++ routes/api.php
@@ -11,43 +11,43 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     // Auth
-    Route::get('auth', [AuthController::class, 'logout']);
-    Route::get('profile', [AuthController::class, 'profile']);
+    Route::get('auth', [AuthController::class, 'logout'])->name("auth.logout");
+    Route::get('profile', [AuthController::class, 'profile'])->name("auth.profile");
 
     // User
     Route::resource('user', UserController::class);
-    Route::post('user/{id}', [UserController::class, 'update']);
+    Route::post('user/{id}', [UserController::class, 'update'])->name("user.update");
     // Route::get('user/roles', [UserController::class, 'rolelist']);
 
     // Roles
-    Route::get('roles', [PermissionController::class, 'rolelist']);
+    Route::get('roles', [PermissionController::class, 'rolelist'])->name("permission.rolelist");
 
     //Products
     Route::resource('menu', MenuController::class);
-    Route::post('menu/{id}', [MenuController::class, 'update']);
+    Route::post('menu/{id}', [MenuController::class, 'update'])->name("menu.update");
 
     // Category
     Route::resource('category', CategoryController::class);
-    Route::get('categories', [CategoryController::class, 'categorylist']);
+    Route::get('categories', [CategoryController::class, 'categorylist'])->name("category.categorylist");
 
     // Order
     Route::resource('order', OrderController::class);
-    Route::get('getcount', [OrderController::class, 'getUnfinishedTransactionCount']);
+    Route::get('getcount', [OrderController::class, 'getUnfinishedTransactionCount'])->name("order.getUnfinishedTransactionCount");
 
     // Dashboard Report
-    Route::get('/report/dashboarddaily', [ReportController::class, 'dashboardDailyReport']);
-    Route::get('/report/dashboardweekly', [ReportController::class, 'dashboardWeeklyReport']);
-    Route::get('/report/dashboardyearly', [ReportController::class, 'dasboardYearlyReport']);
-    Route::get('/report/dashboardrecenttransaction', [ReportController::class, 'dashboardRecentTransaction']);
+    Route::get('/report/dashboarddaily', [ReportController::class, 'dashboardDailyReport'])->name("report.dashboardDailyReport");
+    Route::get('/report/dashboardweekly', [ReportController::class, 'dashboardWeeklyReport'])->name("report.dashboardWeeklyReport");
+    Route::get('/report/dashboardyearly', [ReportController::class, 'dasboardYearlyReport'])->name("report.dasboardYearlyReport");
+    Route::get('/report/dashboardrecenttransaction', [ReportController::class, 'dashboardRecentTransaction'])->name("report.dashboardRecentTransaction");
 
     // Report
-    Route::get('/report/daily', [ReportController::class, 'dailyReport']);
-    Route::get('/report/weekly', [ReportController::class, 'weeklyReport']);
-    Route::get('/report/monthly', [ReportController::class, 'monthlyReport']);
-    Route::get('/report/yearly', [ReportController::class, 'yearlyReport']);
-    Route::get('/report/alltransaction', [ReportController::class, 'allTransactionReport']);
-    Route::get('/report/exportcustom', [ReportController::class, 'exportcustom']);
+    Route::get('/report/daily', [ReportController::class, 'dailyReport'])->name("report.dailyReport");
+    Route::get('/report/weekly', [ReportController::class, 'weeklyReport'])->name("report.weeklyReport");
+    Route::get('/report/monthly', [ReportController::class, 'monthlyReport'])->name("report.monthlyReport");
+    Route::get('/report/yearly', [ReportController::class, 'yearlyReport'])->name("report.yearlyReport");
+    Route::get('/report/alltransaction', [ReportController::class, 'allTransactionReport'])->name("report.allTransactionReport");
+    Route::get('/report/exportcustom', [ReportController::class, 'exportcustom'])->name("report.exportcustom");
 });
 
 //Login
-Route::post('auth', [AuthController::class, 'login']);
+Route::post('auth', [AuthController::class, 'login'])->name("auth.login");
